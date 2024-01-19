32173ad733995ac7d7a32f670f2bbb1a5405a3ce
botond.nagy <botond.nagy@egroup.hu>
2024-01-19 17:50:39 +0100

    test

--- api/calendly.js
+++ api/calendly.js
@@ -21,7 +21,7 @@ const formattedLastDay = (lastDay.getMonth() + 1).toString().padStart(2, '0') + 
     // if (!response.ok) {
     //     throw new Error(`Error: ${response.statusText}`);
     // }
-    const response = await fetch(`https://api.calendly.com/user_busy_times?user=https%3A%2F%2Fapi.calendly.com%2Fusers%2F93997760-d172-458b-b67b-69d6f0f31ee7&start_time=2024-01-01T20%3A00%3A00.000000Z&end_time=2024-01-07T24%3A00%3A00.000000Z`, { headers: {'Authorization': `Bearer ${calendlyApiKey}`} })
+    const response = await fetch(`https://api.calendly.com/user_busy_times?user=https%3A%2F%2Fapi.calendly.com%2Fusers%2F93997760-d172-458b-b67b-69d6f0f31ee7&start_time=2024-01-09T20%3A00%3A00.000000Z&end_time=2024-01-15T24%3A00%3A00.000000Z`, { headers: {'Authorization': `${calendlyApiKey}`} })
     if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
     }
