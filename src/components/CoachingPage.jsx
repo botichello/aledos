ef149310a10610d275b685960e7f64e22645b30b
botond.nagy <botond.nagy@egroup.hu>
2024-01-19 17:22:10 +0100

    test

--- src/components/CoachingPage.jsx
+++ src/components/CoachingPage.jsx
@@ -53,9 +53,7 @@ export default function CoachingPage() {
       // };
       try {
         // Replace '/api/calendly' with the path to your serverless function
-        const response = await fetch(
-          "https://aledos-git-development-botichellos-projects.vercel.app/api/calendly"
-        );
+        const response = await fetch("https://aledos.lol/api/calendly");
         if (!response.ok) {
           throw new Error(`Error: ${response.statusText}`);
         }
