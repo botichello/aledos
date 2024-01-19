355489a0f51e674ae89598f9c4b6ea8b46c8192f
botond.nagy <botond.nagy@egroup.hu>
2024-01-19 17:42:53 +0100

    test

--- api/calendly.js
+++ api/calendly.js
@@ -1,14 +1,8 @@
 // api/calendly.js
-import axios from 'axios';
 
 
 export default async (req, res) => {
-    const calendlyApiKeyT = process.env.CALENDLY_API_KEY; // Access the environment variable
-    console.log(calendlyApiKeyT)
-    const calendlyApiKey = 'eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNzA1NjA2MzIzLCJqdGkiOiJkM2JlMDhhOS00ZDQxLTRlMWUtYmMwOC04OTI5MDliNDczZTciLCJ1c2VyX3V1aWQiOiI5Mzk5Nzc2MC1kMTcyLTQ1OGItYjY3Yi02OWQ2ZjBmMzFlZTcifQ.-yixkq4bPbIrOirywQvLwTUPCn7PfBg_ydkjgYjSwhJ0eldYsGbM5ivcpKfqwonfU4Q-Rc5j078TpH2HnNvG7g';
-    console.log(calendlyApiKey)
-    const now = new Date();
-
+    const calendlyApiKey = process.env.CALENDLY_API_KEY; // Access the environment variable
 // Get the current year and month
 const year = now.getFullYear();
 const month = now.getMonth(); // Note: January is 0, December is 11
@@ -23,7 +17,11 @@ const lastDay = new Date(year, month + 1, 0);
 const formattedLastDay = (lastDay.getMonth() + 1).toString().padStart(2, '0') + '-' + lastDay.getDate().toString().padStart(2, '0');
 
     try {
-    const response = await axios.get(`https://api.calendly.com/user_busy_times?user=https%3A%2F%2Fapi.calendly.com%2Fusers%2F93997760-d172-458b-b67b-69d6f0f31ee7&start_time=2024-${formattedFirstDay}T20%3A00%3A00.000000Z&end_time=2024-${formattedLastDay}T24%3A00%3A00.000000Z`, { headers: { Authorization: `Bearer ${calendlyApiKey}` } })
+    // const response = await axios.get(`https://api.calendly.com/user_busy_times?user=https%3A%2F%2Fapi.calendly.com%2Fusers%2F93997760-d172-458b-b67b-69d6f0f31ee7&start_time=2024-${formattedFirstDay}T20%3A00%3A00.000000Z&end_time=2024-${formattedLastDay}T24%3A00%3A00.000000Z`, { headers: { Authorization: `Bearer ${calendlyApiKey}` } })
+    // if (!response.ok) {
+    //     throw new Error(`Error: ${response.statusText}`);
+    // }
+    const response = await fetch(`https://api.calendly.com/user_busy_times?user=https%3A%2F%2Fapi.calendly.com%2Fusers%2F93997760-d172-458b-b67b-69d6f0f31ee7&start_time=2024-01-01T20%3A00%3A00.000000Z&end_time=2024-01-07T24%3A00%3A00.000000Z`, { headers: {'Authorization': `Bearer ${calendlyApiKey}`} })
     if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
     }
