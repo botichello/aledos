df9e782fe39bf16e992a13ccf5193257e934f9cf
botond.nagy <botond.nagy@egroup.hu>
2024-01-19 17:08:43 +0100

    test

--- api/calendly.js
+++ api/calendly.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 
 
 export default async (req, res) => {
-    //const calendlyApiKey = 'eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNzA1NjA2MzIzLCJqdGkiOiJkM2JlMDhhOS00ZDQxLTRlMWUtYmMwOC04OTI5MDliNDczZTciLCJ1c2VyX3V1aWQiOiI5Mzk5Nzc2MC1kMTcyLTQ1OGItYjY3Yi02OWQ2ZjBmMzFlZTcifQ.-yixkq4bPbIrOirywQvLwTUPCn7PfBg_ydkjgYjSwhJ0eldYsGbM5ivcpKfqwonfU4Q-Rc5j078TpH2HnNvG7g';
-    const calendlyApiKey = process.env.CALENDLY_API_KEY; // Access the environment variable
+    const calendlyApiKeyT = process.env.CALENDLY_API_KEY; // Access the environment variable
+    console.log(calendlyApiKeyT)
+    const calendlyApiKey = 'eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNzA1NjA2MzIzLCJqdGkiOiJkM2JlMDhhOS00ZDQxLTRlMWUtYmMwOC04OTI5MDliNDczZTciLCJ1c2VyX3V1aWQiOiI5Mzk5Nzc2MC1kMTcyLTQ1OGItYjY3Yi02OWQ2ZjBmMzFlZTcifQ.-yixkq4bPbIrOirywQvLwTUPCn7PfBg_ydkjgYjSwhJ0eldYsGbM5ivcpKfqwonfU4Q-Rc5j078TpH2HnNvG7g';
+    console.log(calendlyApiKey)
     const now = new Date();
 
 // Get the current year and month
