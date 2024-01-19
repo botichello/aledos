19f6df08a74f3ca1f5852c0a8788215be0ed462a
botond.nagy <botond.nagy@egroup.hu>
2024-01-19 17:55:20 +0100

    t

--- api/calendly.js
+++ api/calendly.js
@@ -4,17 +4,6 @@
 export default async (req, res) => {
     const calendlyApiKey = process.env.CALENDLY_API_KEY; // Access the environment variable
 // Get the current year and month
-const year = now.getFullYear();
-const month = now.getMonth(); // Note: January is 0, December is 11
-
-// Get the first day of the current month
-const firstDay = new Date(year, month, 1);
-const formattedFirstDay = (firstDay.getMonth() + 1).toString().padStart(2, '0') + '-' + firstDay.getDate().toString().padStart(2, '0');
-
-// Get the last day of the current month
-// We set the date to the first day of the next month and then go one day back
-const lastDay = new Date(year, month + 1, 0);
-const formattedLastDay = (lastDay.getMonth() + 1).toString().padStart(2, '0') + '-' + lastDay.getDate().toString().padStart(2, '0');
 
     try {
     // const response = await axios.get(`https://api.calendly.com/user_busy_times?user=https%3A%2F%2Fapi.calendly.com%2Fusers%2F93997760-d172-458b-b67b-69d6f0f31ee7&start_time=2024-${formattedFirstDay}T20%3A00%3A00.000000Z&end_time=2024-${formattedLastDay}T24%3A00%3A00.000000Z`, { headers: { Authorization: `Bearer ${calendlyApiKey}` } })
