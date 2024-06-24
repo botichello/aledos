e04a0e20ec0e8ffb8ba0aba9b1a7b783bf74a8ce
botond.nagy <botond.nagy@egroup.hu>
2024-06-24 18:01:41 +0200

    started freecoaching, added footer to coachingpage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import CoachingPage from './components/CoachingPage.jsx';
-import { ScrollProvider } from '../src/components/ScrollContext';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import CoachingPage from "./components/CoachingPage.jsx";
+import FreeCoachingPage from "./components/FreeCoachingPage.jsx";
+import { ScrollProvider } from "../src/components/ScrollContext";
 function App() {
   return (
     <>
       <ScrollProvider>
         <Routes>
-          <Route path="/" element={<HomePage/>}></Route>
-          <Route path="/coaching" element={<CoachingPage/>}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/coaching" element={<CoachingPage />}></Route>
+          {/* <Route path="/freecoaching" element={<FreeCoachingPage />}></Route> */}
         </Routes>
       </ScrollProvider>
     </>
